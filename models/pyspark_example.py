ac550a73f95cd95243ab39ce4ae4394e87666429
Sung Won Chung <sungwonchung3@gmail.com>
2024-06-05 13:57:03 -0700

    working version

--- models/pyspark_example.py
+++ models/pyspark_example.py
@@ -5,11 +5,17 @@ import pandas as pd
 from pyspark.sql import DataFrame, functions
 
 from sqlmesh import ExecutionContext, model
+from sqlglot.expressions import to_column
+
 
 @model(
     "demo.pyspark_amount_by_status",
     cron="@daily",
     grain="status",
+    audits=[
+        ("unique_values", {"columns": [to_column("status")]}),
+        ("not_null", {"columns": [to_column("status")]}),
+    ],
     columns={
         "status": "text",
         "total_amount": "float",
@@ -26,7 +32,7 @@ def execute(
     table = context.table("demo.orders")
 
     # use spark to group by status and compute the total amount
-    df = context.spark.table(table).groupBy("status").agg(functions.sum("amount").alias("total_amount"))
+    df = context.spark.table(table).filter("status IS NOT NULL").groupBy("status").agg(functions.sum("amount").alias("total_amount"))
 
     # returns the pyspark DataFrame directly, so no data is computed locally
     return df
@@ -34,3 +40,4 @@ def execute(
 # sqlmesh create_test demo.pyspark_amount_by_status --query demo.orders "select * from demo.orders limit 5"
 # 
 # unset GOOGLE_CREDENTIALS when running this project 
+# this requires databricks connect for this pyspark dataframe to work properly
