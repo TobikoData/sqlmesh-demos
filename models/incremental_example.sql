c260facfe897df0c23fbd856e578aed2bf531f50
Sung Won Chung <sungwonchung3@gmail.com>
2024-06-21 10:31:24 -0700

    update formats

--- models/incremental_example.sql
+++ models/incremental_example.sql
@@ -1,8 +1,7 @@
 MODEL (
-  name demo.incremental_example,
+  name demo.incremental_databricks_example,
   kind INCREMENTAL_BY_TIME_RANGE (
-    time_column event_timestamp,
-    on_destructive_change error,
+    time_column (event_timestamp, '%Y-%m-%dT%H:%M:%S'),
     lookback 2, -- handle late arriving events for the past 2 (2*1) days based on cron interval
     forward_only true -- All changes will be forward only
   ),
@@ -20,18 +19,18 @@ MODEL (
 -- step 1: `sqlmesh plan dev` to create this model for the first time and backfill for all of history
 -- step 2: add a new column
 -- step 3: pick a start date to backfill like: '2024-06-18'
--- step 4: validate only a portion of rows were backfilled: `sqlmesh fetchdf "select * from demo__dev_sung.incremental_example"`
+-- step 4: validate only a portion of rows were backfilled: `sqlmesh fetchdf "select * from demo__dev_sung.incremental_databricks_example"`
 -- step 5: `sqlmesh plan` to promote to prod with a virtual update, note: the dev backfill preview won't be reused for promotion and is only for dev purposes
--- step 6: `sqlmesh plan --restate-model "demo.incremental_example"`, to invoke a backfill to mirror dev's data preview
+-- step 6: `sqlmesh plan --restate-model "demo.incremental_databricks_example"`, to invoke a backfill to mirror dev's data preview
 -- step 7: pick the same backfill start date for prod as dev's above: '2024-06-18'
--- step 8: validate changes to prod: `sqlmesh fetchdf "select * from demo.incremental_example"`
+-- step 8: validate changes to prod: `sqlmesh fetchdf "select * from demo.incremental_databricks_example"`
 
 SELECT
   event_id,
   event_name,
-  event_timestamp,
+  event_timestamp::TIMESTAMP as event_timestamp, -- removing timezone offset
   user_id,
   IF(event_name = 'video_view', 'high', 'low') AS user_intent_level, --
 FROM public_demo.raw_data.demo_events --external model, automatically generate yaml using command: `sqlmesh create_external_models` 
 WHERE
-  event_timestamp BETWEEN @start_date AND @end_date
+  event_timestamp BETWEEN @start_ts AND @end_ts -- use the correct time format: https://sqlmesh.readthedocs.io/en/stable/concepts/macros/macro_variables/#temporal-variables
