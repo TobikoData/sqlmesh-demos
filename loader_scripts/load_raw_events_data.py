7c8a7d9edbd0de20a88c8f239c9fb964b41963f6
Sung Won Chung <sungwonchung3@gmail.com>
2024-06-20 19:19:20 -0700

    working loader

--- loader_scripts/load_raw_events_data.py
+++ loader_scripts/load_raw_events_data.py
@@ -3,14 +3,12 @@ from pyspark.sql.types import StructType, StructField, StringType, TimestampType
 from pyspark.sql import Row
 from faker import Faker
 import uuid
-from datetime import datetime
+from datetime import datetime, timedelta
 import random
-
-# Configure Databricks Connect
 import os
 from databricks.connect import DatabricksSession
 
-
+# Configure Databricks Connect
 server_hostname = "dbc-b9f590c4-0a08.cloud.databricks.com"
 cluster_id = "0603-211256-ns7ii2e2"
 
@@ -19,9 +17,6 @@ dbc = DatabricksSession.builder.remote(
   token      = os.getenv('DATABRICKS_ACCESS_TOKEN'),
   cluster_id = cluster_id
 ).getOrCreate()
-
-
-fake = Faker()
 
 # Define the list of possible event names
 event_names = ["page_view", "product_view", "ad_view", "video_view", "blog_view"]
@@ -34,12 +29,14 @@ schema = StructType([
     StructField("user_id", StringType(), True)
 ])
 
-def generate_fake_data(num_rows: int):
+
+def generate_fake_data(num_rows: int, end_date: str):
+    end_date_parsed = datetime.strptime(end_date, '%Y-%m-%d')
     data = []
-    for _ in range(num_rows):
+    for i in range(num_rows):
         event_id = str(uuid.uuid4())
         event_name = random.choice(event_names)
-        event_timestamp = datetime.now()
+        event_timestamp = end_date_parsed - timedelta(days=i)
         user_id = str(uuid.uuid4())
         row = Row(event_id=event_id, event_name=event_name, event_timestamp=event_timestamp, user_id=user_id)
         data.append(row)
@@ -50,9 +47,9 @@ def create_schema_if_not_exists(schema_name: str):
     dbc.sql(f"CREATE SCHEMA IF NOT EXISTS {schema_name}")
 
 
-def append_to_databricks_table(table_name: str, num_rows: int):
-    # Generate 5 rows of fake data
-    fake_data = generate_fake_data(num_rows)
+def append_to_databricks_table(table_name: str, num_rows: int, end_date: str):
+    # Generate fake data
+    fake_data = generate_fake_data(num_rows, end_date)
     
     # Create a Spark DataFrame
     df = dbc.createDataFrame(fake_data, schema)
@@ -64,7 +61,7 @@ def append_to_databricks_table(table_name: str, num_rows: int):
     # Append the data to the Databricks catalog
     df.write.mode("append").saveAsTable(table_name)
 
-    print(f"{num_rows} rows of raw events demo data appended to {table_name}")
+    print(f"{num_rows} rows of raw events demo data ending at {end_date} appended to {table_name}")
 
 # Call the function to append data
-append_to_databricks_table(table_name="public_demo.raw_data.demo_events", num_rows=3)
+append_to_databricks_table(table_name="public_demo.raw_data.demo_events", num_rows=3, end_date="2024-06-20")
